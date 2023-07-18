fc069a309361a4edc6dec1fc82466ab9f98b1967
Unknown-60 <47797936+Unknown-60@users.noreply.github.com>
2023-07-17 22:07:11 -0300

    Add files via upload

---
+++ app/src/main/java/com/unknown60/entityencoder/utils/Utility.java
@@ -0,0 +1,49 @@
+package com.unknown60.entityencoder.utils;
+
+import android.content.Context;
+import android.os.Build;
+import java.lang.reflect.Method;
+
+public class Utility {
+
+	public static boolean isChrome() {
+		return Build.BRAND.equals("chromium") || Build.BRAND.equals("chrome");
+	}
+
+	public static int getStatusBarHeight(Context context) {
+		int result = 0;
+		int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+		if (resourceId > 0) {
+			result = context.getResources().getDimensionPixelSize(resourceId);
+		}
+		return result;
+	}
+
+	public static String getFirstCharacter(String sentence) {
+		for (int i = 0; i < sentence.length(); i++) {
+			String s = sentence.substring(i, i+1);
+			if (s.equals("[") || s.equals("]")) continue;
+			if (s.equals("{") || s.equals("}")) continue;
+			if (s.equals("(") || s.equals(")")) continue;
+			if (s.equals(",") || s.equals(".")) continue;
+			if (s.equals("<") || s.equals(">")) continue;
+			if (s.equals("《") || s.equals("》")) continue;
+			if (s.equals("【") || s.equals("】")) continue;
+			if (s.equals("｛") || s.equals("｝")) continue;
+			return s;
+		}
+		return null;
+	}
+
+	public static String getSystemProperties(String key) {
+		try {
+			Class c = Class.forName("android.os.SystemProperties");
+			Method m = c.getDeclaredMethod("get", String.class);
+			m.setAccessible(true);
+			return (String) m.invoke(null, key);
+		} catch (Throwable e) {
+			return "";
+		}
+	}
+
+}
